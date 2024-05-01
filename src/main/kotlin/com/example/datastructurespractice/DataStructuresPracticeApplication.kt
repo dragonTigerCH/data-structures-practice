f747b3438d1522a6de6d780b2a4b57f38291b423
dragonTigerChoi <yongho@koreaspacedata.com>
2024-05-01 14:10:50 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/datastructurespractice/DataStructuresPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.datastructurespractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DataStructuresPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<DataStructuresPracticeApplication>(*args)
+}
